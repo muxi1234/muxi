322a8f004e9a17a9e7f083af252830b9ce454b68
cheng <45452968+muxi1234@users.noreply.github.com>
2021-06-13 17:59:08 +0800

    Create FGOB.sol

---
+++ FGOB.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+
+
+pragma solidity^0.8.0;
+
+
+
+import "https://github.com/blockblock0/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract ExampleTokene is ERC20("ExampleToken","ET") {
+
+}
+
+contract BLOCKYUYUYUYUYU is ERC20 {
+
+constructor() ERC20("BLOCKYUYUYUYUYU", "FGOB") {
+
+_mint(msg.sender, 800000000000000000000000000);
+
+}
+
+}
